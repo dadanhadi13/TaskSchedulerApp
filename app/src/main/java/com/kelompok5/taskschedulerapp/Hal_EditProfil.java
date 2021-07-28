607aaad9dbea0c42d8149f8d5ce5fa9c7e9df31f
Kavin Harriz <tugaskavinharriz@gmail.com>
2021-07-28 21:56:29 +0700

    EditProfile-Kafin

---
+++ app/src/main/java/com/kelompok5/taskschedulerapp/Hal_EditProfil.java
@@ -0,0 +1,13 @@
+package com.kelompok5.taskschedulerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hal_EditProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofil);
+    }
+}
