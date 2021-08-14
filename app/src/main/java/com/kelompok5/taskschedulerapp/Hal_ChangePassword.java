fe168b3fe3d7b7ab94b05f7727f9719ee3da32bc
Candra-dev <84716211+Candra-dev@users.noreply.github.com>
2021-08-14 22:53:17 +0700

    penambahan notifikasi

--- app/src/main/java/com/kelompok5/taskschedulerapp/Hal_ChangePassword.java
+++ app/src/main/java/com/kelompok5/taskschedulerapp/Hal_ChangePassword.java
@@ -3,15 +3,11 @@ package com.kelompok5.taskschedulerapp;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-class Hal_changePassword extends AppCompatActivity {
+public class Hal_ChangePassword extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.changepassword);
     }
-
-
 }
-
-
